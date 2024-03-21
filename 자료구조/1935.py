b2a170e7e1b869181bdce186a361bba2803135e7
LEEHYEYEONG <elto789@naver.com>
2024-03-21 21:55:14 +0900

    fix: 숫자 할당 부분 수정 - 딕셔너리 사용

--- 자료구조/1935.py
+++ 자료구조/1935.py
@@ -1,25 +1,24 @@
-from collections import deque
 import sys
 
 input = sys.stdin.readline
 n = int(input())
-array = deque(list(input().strip()))
-num = deque([int(input()) for _ in range(n)])
+array = list(input().strip())
+num_dict = dict()
+
+for i in range(n):
+    num = int(input())
+    num_dict[chr(65 + i)] = num
+
+for i in range(len(array)):
+    if array[i] in num_dict:
+        array[i] = num_dict[array[i]]
 
 
 def delete(array, value, i):
     array.insert(i + 1, value)
-    del array[i]
-    del array[i - 1]
-    del array[i - 2]
+    del array[i - 2 : i + 1]
     return array
 
-
-for i in range(len(array)):
-    if array[i].isalpha():
-        array[i] = num.popleft()
-    else:
-        continue
 
 while len(array) != 1:
     for i in range(len(array)):
@@ -41,3 +40,29 @@ while len(array) != 1:
             break
 
 print("{:.2f}".format(array[0]))
+
+# 스택사용
+n = int(input())
+word = list(input())
+num = [0] * n
+
+for i in range(n):  # 피연산자 숫자 입력
+    num[i] = int(input())
+
+stack = []
+
+for i in word:
+    if "A" <= i <= "Z":
+        stack.append(num[ord(i) - ord("A")])
+    else:  # 연산자일시
+        str2 = stack.pop()
+        str1 = stack.pop()
+        if i == "+":
+            stack.append(str1 + str2)
+        elif i == "-":
+            stack.append(str1 - str2)
+        elif i == "*":
+            stack.append(str1 * str2)
+        elif i == "/":
+            stack.append(str1 / str2)
+print("%.2f" % stack[0])
